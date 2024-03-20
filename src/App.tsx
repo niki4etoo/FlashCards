9fd6d027a58bf2eb1239c26b6a8e420fc1da3c9a
Nikolai Nanev <niki4etooo@gmail.com>
2024-03-20 09:44:05 +0200

    added flashcards component with the example of a card

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,9 @@ import {
 } from 'react-router-dom'
 
 import './App.css'
-import { useState } from 'react';
+import FlashCards from './components/FlashCards';
 
 function App() {
-
-  let [isFlip, setIsFlip] = useState(false);
-
-  const handleClick = () => {
-    setIsFlip(isFlip = !isFlip);
-  }
-
   return (
     <>
       <div className='container'>
@@ -22,32 +15,7 @@ function App() {
           <Link to="/login">Login</Link>
           <Link to="/about">About</Link>
         </nav>
-        <div className='flashcard' onClick={handleClick}>
-          {isFlip &&
-            <div className='flashcard-back'>
-              <div id='flashcard-header'>
-                仕事 - working, labor, occupation
-              </div>
-              <div id='flashcard-description'>
-                し ご と ( shigoto )
-              </div>
-            </div>
-          }
-          {
-            !isFlip &&
-            <div className='flashcard-front'>
-              <div id='flashcard-header'>
-                仕事
-              </div>
-              <div id='flashcard-description'>
-                し ご と ( shigoto )
-              </div>
-            </div>
-          }
-
-
-
-        </div>
+        <FlashCards />
       </div>
     </>
   )
--- src/components/FlashCards.tsx
+++ src/components/FlashCards.tsx
@@ -1,6 +1,40 @@
+import { useState } from 'react';
+
 const FlashCards = () => {
+
+    let [isFlip, setIsFlip] = useState(false);
+
+    const handleClick = () => {
+        setIsFlip(isFlip = !isFlip);
+    }
+
     return (
-        <>To do</>
+        <div className='flashcard' onClick={handleClick}>
+            {isFlip &&
+                <div className='flashcard-back'>
+                    <div id='flashcard-header'>
+                        仕事 - working, labor, occupation
+                    </div>
+                    <div id='flashcard-description'>
+                        し ご と ( shigoto )
+                    </div>
+                </div>
+            }
+            {
+                !isFlip &&
+                <div className='flashcard-front'>
+                    <div id='flashcard-header'>
+                        仕事
+                    </div>
+                    <div id='flashcard-description'>
+                        し ご と ( shigoto )
+                    </div>
+                </div>
+            }
+
+
+
+        </div>
     )
 }
 
