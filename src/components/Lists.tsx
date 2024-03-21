ba8ddfcf9387986f41b58bea12e8605b1b06b0ea
Nikolai Nanev <niki4etooo@gmail.com>
2024-03-21 09:45:16 +0200

    added list form input

--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -1,9 +1,30 @@
 import { Back } from './Navigation'
 
 const Lists = () => {
+
+    const addList = (e: any) => {
+        e.preventDefault();
+        // Adding list ( to do )
+        console.log('Here we are');
+        
+    }
+
     return (
         <>
-        <Back />
+            <Back />
+            <form>
+                <div>
+                    <h3>Title:</h3>
+                    <input name='title' />
+                </div>
+                <div>
+                    <h3>Description:</h3>
+                    <textarea name="desc" id="desc" cols={30} rows={10}></textarea>
+                </div>
+                <div>
+                    <input type='submit' value={"Add"} onClick={(e) => addList(e)} />
+                </div>
+            </form>
         </>
     )
 }
