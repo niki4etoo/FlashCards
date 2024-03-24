c768547994398f0b534cea26204b17572e563a20
Nikolai Nanev <niki4etooo@gmail.com>
2024-03-24 21:59:05 +0200

    added switch button for front/back of the flashcard

--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Back } from './Navigation'
 
 const Lists = () => {
@@ -6,25 +7,37 @@ const Lists = () => {
         e.preventDefault();
         // Adding list ( to do )
         console.log('Here we are');
-        
+
+    }
+
+    const [isBack, setIsBack] = useState(true);
+
+    const switchFrontBack = () => {
+        // Switch to back of the flash card
+
+        setIsBack(!isBack);
     }
 
     return (
         <>
             <Back />
-            <form>
-                <div>
-                    <h3>Title:</h3>
-                    <input name='title' />
-                </div>
-                <div>
-                    <h3>Description:</h3>
-                    <textarea name="desc" id="desc" cols={30} rows={10}></textarea>
-                </div>
-                <div>
-                    <input type='submit' value={"Add"} onClick={(e) => addList(e)} />
-                </div>
-            </form>
+            <div>
+                <form>
+                    <div>
+                        <h3>Title:</h3>
+                        <input name='title' />
+                    </div>
+                    <div>
+                        <h3>Description:</h3>
+                        <textarea name="desc" id="desc" cols={30} rows={10}></textarea>
+                    </div>
+                    <div className='buttons'>
+                        <input type='submit' value={"Add"} onClick={(e) => addList(e)} />
+                        <input type='button' value={isBack ? "Flashcard Back" : "Flashcard Front"} onClick={switchFrontBack} />
+                    </div>
+                </form>
+            </div>
+
         </>
     )
 }
